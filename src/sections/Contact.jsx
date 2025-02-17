2c977b44d9d5c3ec84a26359e949b24e92c0eb36
jega221 <asjega11@gmail.com>
2025-02-17 11:20:48 +0200

    main

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,7 +1,7 @@
 import emailjs from '@emailjs/browser';
 import { useRef, useState } from 'react';
 
-import useAlert from '../hooks/useAlert.js';
+import useAlert from "../hooks/useAlert.js";
 import Alert from '../components/Alert.jsx';
 
 const Contact = () => {
